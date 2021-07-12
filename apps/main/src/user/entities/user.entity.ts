496be515e624278f4ea73a61939a9a23ed19ccee
imcvampire <quocanh96@gmail.com>
2021-07-12 21:38:28 +0700

    feat(*): init graphql

--- apps/main/src/user/entities/user.entity.ts
+++ apps/main/src/user/entities/user.entity.ts
@@ -1,9 +1,10 @@
-import { ObjectType } from '@nestjs/graphql';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
 import { BaseEntity, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
 @Entity()
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
+  @Field(() => ID)
   id: number;
 }
