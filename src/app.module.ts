e52162fa7bfb76551ca56af25306199eb8b12503
imcvampire <quocanh96@gmail.com>
2021-07-13 22:21:11 +0700

    feat(*): init tools

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getConnectionOptions } from 'typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          autoLoadEntities: true,
+        }),
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+  ],
 })
 export class AppModule {}
