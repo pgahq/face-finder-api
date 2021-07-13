51b2c4f30c349e57289677448e10640b03352fe3
hongtt <261096tr@gmail.com>
2021-07-13 21:28:02 +0700

    rm migrations

--- apps/main/migration/1626080440790-User.ts
+++ apps/main/migration/1626080440790-User.ts
@@ -1,15 +1,45 @@
-import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table, TableIndex } from 'typeorm';
 
 export class User1626080440790 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    return queryRunner.createTable(
-      new Table({
-        name: 'users',
-      }),
-    );
+    await queryRunner.createTable(new Table({
+      name: "user",
+      columns: [
+          {
+            name: "id",
+            type: "serial",
+            isPrimary: true
+          },
+          {
+            name: "username",
+            type: "varchar",
+          },
+          {
+            name: "password",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            isNullable: true
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            isNullable: true,
+          }
+      ]
+  }), true);
+
+  await queryRunner.createIndex('user', new TableIndex({
+    name: "IDX_USER_NAME",
+    columnNames: ["username"]
+  }));
+
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    return queryRunner.dropTable('users');
+    await queryRunner.dropIndex("user", "IDX_USER_NAME");
+    await queryRunner.dropTable("user");
   }
 }
