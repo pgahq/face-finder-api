be86fd93500c009450b6fe97670a38161049574e
imcvampire <quocanh96@gmail.com>
2021-07-13 22:51:26 +0700

    feat(*): setup migration

---
+++ ormconfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'main',
+  migrations: ['dist/migration/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/migration',
+  },
+};
