39e4759037a94b4559b7e6d210c31c4d86ed264c
imcvampire <quocanh96@gmail.com>
2021-07-14 15:11:03 +0000

    feat(*): change to direnv

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,42 +2,25 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { createConnection } from 'typeorm';
+import { createConnection, getConnectionOptions } from 'typeorm';
 
 import { UserModule } from './user/user.module';
-import databaseConfiguration from './config/database.config';
-import authConfiguration from './config/auth.config';
+import { authConfig } from './config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [authConfig],
+    }),
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('database.host'),
-        port: +configService.get<number>('database.port'),
-        username: configService.get('database.username'),
-        password: configService.get('database.password'),
-        database: configService.get('database.name'),
-        migrations: ['dist/migration/*{.ts,.js}'],
-        cli: {
-          migrationsDir: 'src/migration',
-        },
-        logging: true,
-        autoLoadEntities: true,
-      }),
-      inject: [ConfigService],
-      connectionFactory: async (options) => {
-        const connection = await createConnection(options);
-        return connection;
-      },
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          autoLoadEntities: true,
+        }),
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
-    }),
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [databaseConfiguration, authConfiguration],
     }),
     UserModule,
   ],
