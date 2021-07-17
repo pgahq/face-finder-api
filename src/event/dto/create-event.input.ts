64bc59819f77e8d2f7a7e15409b5d3a8c0cc5590
Valeria Tran <261096tr@gmail.com>
2021-07-17 20:09:26 +0700

    Update create-event.input.ts
    Remove non-descriptive descriptions in create-event.input

--- src/event/dto/create-event.input.ts
+++ src/event/dto/create-event.input.ts
@@ -2,15 +2,15 @@ import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
 export class CreateEventInput {
-  @Field(() => String, { description: 'Name of event' })
+  @Field(() => String)
   name: string;
 
-  @Field(() => Date, { description: 'Start time of event' })
+  @Field(() => Date)
   start_time: Date;
 
-  @Field(() => Date, { description: 'End time of event' })
+  @Field(() => Date)
   end_time: Date;
 
-  @Field(() => String, { description: 'Link to photo storage of event' })
+  @Field(() => String)
   gcs_bucket: string;
 }
