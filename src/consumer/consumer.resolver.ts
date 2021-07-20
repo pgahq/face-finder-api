aaee867ac438d228b53d00af400e01ff1fddcdd1
hongtt <261096tr@gmail.com>
2021-07-20 17:25:46 +0000

    use compreface sdk

--- src/consumer/consumer.resolver.ts
+++ src/consumer/consumer.resolver.ts
@@ -2,7 +2,7 @@ import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { Consumer } from 'consumer/entitites/consumer.entity';
 import axios from 'axios';
 import { FileUpload, GraphQLUpload } from 'graphql-upload';
-import FormData = require("form-data");
+import { CompreFace } from '@exadel/compreface-js-sdk';
 
 
 @Resolver()
@@ -19,23 +19,31 @@ export class ConsumerResolver {
     const {filename, mimetype, encoding, createReadStream} = selfie
     
     const blob = await streamToString(createReadStream())
+
+    let api_key = "00000000-0000-0000-0000-000000000002";
+    let url = "http://localhost";
+    let port = 8000;
+
+    let compreFace = new CompreFace(url, port); // set CompreFace url and port 
+    let recognitionService = compreFace.initFaceRecognitionService(api_key); // initialize service
+    let faceCollection = recognitionService.getFaceCollection(); // use face collection to fill it with known faces
+    console.log(faceCollection)
+    // const formData = new FormData()
+    // formData.append('file', blob)
     
-    const formData = new FormData()
-    formData.append('file', blob)
-    
-    axios({
-      method: 'post',
-      url: 'http://localhost:8000/api/v1/recognition/faces?subject=1234',
-      headers: {
-        'x-api-key': '00000000-0000-0000-0000-000000000002'
-      },
-      data: formData
-    })
-    .then(function (response) {
-      console.log(response.data)
-    }).catch(function(error) {
-      console.log(error.response.data);
-    });
+    // axios({
+    //   method: 'post',
+    //   url: 'http://localhost:8000/api/v1/recognition/faces?subject=1234',
+    //   headers: {
+    //     'x-api-key': '00000000-0000-0000-0000-000000000002'
+    //   },
+    //   data: formData
+    // })
+    // .then(function (response) {
+    //   console.log(response.data)
+    // }).catch(function(error) {
+    //   console.log(error.response.data);
+    // });
     return consumer
   }
 }
