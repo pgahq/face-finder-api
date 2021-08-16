425b9d57e65d3e2a703beeab6853959a075db899
hongtt <261096tr@gmail.com>
2021-08-16 19:13:41 +0000

    feat(api-key): auth for serverless newPhoto

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -3,4 +3,5 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('auth', () => ({
   jwtSecret: process.env.JWT_SECRET,
   expiresIn: process.env.EXPIRES_IN,
+  apiKey: process.env.API_KEY,
 }));
